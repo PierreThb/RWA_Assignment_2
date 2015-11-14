6df2db694211be27e8e1bc503da903e61fb93a49
AzariasB <azarias.boutin@gmail.com>
2015-11-14 16:52:35 +0000

    Remove pictures Creating the project's base

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RWA_Assignment_2
-ezezezeezezezeze
+
+This aim of this project is to create a mobile web application with jquery mobile
+
+The application must include the following features : 
+
+[] Reading a json feed from a wordpress blog
+[] Using /flickr/ API to display a set of images
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Champagne houses</title>
+        
+        <!-- To be changed with the custome versions of jquery-mobile -->
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+    </head>
+    <body>
+    </body>
+</html>
