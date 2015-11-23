25b1068deaa0f48b486c780fd6215c895b254d47
PierreThb <thube.pierre@gmail.com>
2015-11-23 12:37:41 +0000

    Agrandir image

--- assets/js/flickr.js
+++ assets/js/flickr.js
@@ -19,12 +19,12 @@ Flickr.prototype = {
             type: 'GET',
             data: {
                 method: 'flickr.people.getPublicPhotos',
-                api_key: '439d3c788d4c522138a62a1a9faf2f10',
+                api_key: 'e9b9fd859bfb0eb780853a74aa165146',
                 user_id: '136111591@N02',
                 format: 'json',
                 nojsoncallback: '1',
-                auth_token: '72157661417747795-2664a1b6c33f3c77',
-                api_sig: '7a9a87389711f553ecd044a5ddc695ff'
+                auth_token: '72157660823674147-674f3f6a43e64fb0',
+                api_sig: '18214db396f4d526bfd722ee6702b3aa'
             }
         }).success(function (data) {
             if (data.stat === 'ok') {
@@ -43,13 +43,44 @@ Flickr.prototype = {
     displayImages:function (imgArr) {
         var $div = $('<div/>', {id: 'photos'});
         $.each(imgArr, function (index, val) {
-            var img = $("<img/>", {
+//            console.log(val);
+            var $my_a = $('<a/>',{
+                'href':'#popup'+val.title,
+                'data-rel':'popup',
+                'data-position-to':"window",
+                'data-transition':'fade'
+            });
+            var $img = $("<img/>", {
+                'class':"popphoto", 
                 src: 'https://farm' + val.farm + '.staticflickr.com/' + val.server + '/' + val.id + '_' + val.secret + '.jpg'
             });
-            $div.append(img);
+            var $popup = $("<div/>",{
+                'data-role':"popup",
+                'id':'popup'+val.title,
+                'data-overlay-theme':"b",
+                'data-theme':"b",
+                'data-corners':"false"
+            });
+            var $aInsidePopup = $("<a/>",{
+                'href':'#',
+                'data-rel':'back',
+                'class':'ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right'
+            }).text("Close");
+            var $enlargedImg = $("<img/>",{
+               'class':'popphoto',
+               'src':'https://farm' + val.farm + '.staticflickr.com/' + val.server + '/' + val.id + '_' + val.secret + '.jpg',
+               'style':'max-height:512px'
+            });
+//            console.log($my_a);
+            
+            $div.append($my_a);
+            $my_a.append($img);
+            $popup.append($aInsidePopup);
+            $popup.append($enlargedImg);
+            
         });
         $("#image").append($div);
     }
 };
 
-window.flickr = new Flickr();
+window.flickr = new Flickr();   
